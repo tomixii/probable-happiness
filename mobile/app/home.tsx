b3f043cbb1f608c8d1140367b4a8a973f7ffe097
Karri Lehtiranta <karri.lehtiranta@visma.com>
2023-11-11 14:03:50 +0200

    Update UI

--- mobile/app/home.tsx
+++ mobile/app/home.tsx
@@ -1,26 +1,47 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Pressable } from 'react-native'
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { Link } from 'expo-router'
+import { colors } from '../styles/colors'
+import PhotoButton from '../components/PhotoButton'
 
-export default function App() {
+const HomeScreen = () => {
+  return (
+    <LinearGradient colors={[colors.main, 'black']} style={styles.container}>
+      <Text style={styles.headerStyle}>Rawcycle</Text>
 
-    return (
-        <View style={styles.container}>
-
-            <Link href="/photo" asChild>
-                <Ionicons name="md-checkmark-circle" size={32} color="green" />
-            </Link>
-
-        </View>
-    )
+      <Link href="/photo" asChild>
+        <PhotoButton />
+      </Link>
+    </LinearGradient>
+  )
 }
 
+export default HomeScreen
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerStyle: {
+    position: 'absolute',
+    fontSize: 36,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    top: 30,
+    width: '100%',
+    color: 'white',
+  },
+  photoIconContainer: {
+    borderRadius: 100,
+    elevation: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 200,
+    width: 200,
+  },
 })
--- mobile/app/photo.tsx
+++ mobile/app/photo.tsx
@@ -1,66 +1,66 @@
 import React, { createRef, useState, useEffect } from 'react'
 import { Camera, CameraType } from 'expo-camera'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import Ionicons from '@expo/vector-icons/Ionicons';
 import axios from 'axios'
+import PhotoButton from '../components/PhotoButton'
 
-export default function App() {
-    const [type, setType] = useState(CameraType.back)
-    const [permission, requestPermission] = Camera.useCameraPermissions()
-    const cameraRef = createRef<Camera>()
+export default function PhotoScreen() {
+  const [type, setType] = useState(CameraType.back)
+  const [permission, requestPermission] = Camera.useCameraPermissions()
+  const cameraRef = createRef<Camera>()
 
-    useEffect(() => {
-        requestPermission()
-    }, [])
+  useEffect(() => {
+    requestPermission()
+  }, [])
 
-    const toggleCameraType = () => {
-        setType((current) =>
-            current === CameraType.back ? CameraType.front : CameraType.back
-        )
-    }
-
-    const takePicture = async () => {
-        console.log('lol')
-        if (cameraRef.current) {
-            const photo = await cameraRef.current.takePictureAsync()
-            console.log(photo)
-            console.log(photo.base64?.length)
-            const response = await axios.post('http://localhost:8080/items', {
-                image: photo.base64,
-            })
-            console.log(response.data)
-        }
-    }
-
-    return (
-        <View style={styles.container}>
-            <Camera style={styles.camera} type={type} ref={cameraRef}>
-                <View>
-                    <TouchableOpacity onPress={toggleCameraType}>
-                        <Text style={styles.text}>Flip Camera</Text>
-                    </TouchableOpacity>
-                </View>
-                <View>
-                    <TouchableOpacity onPress={takePicture}>
-                        <Text style={styles.text}>Take picture</Text>
-                    </TouchableOpacity>
-                </View>
-            </Camera>
-        </View>
+  const toggleCameraType = () => {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
     )
+  }
+
+  const takePicture = async () => {
+    console.log('lol')
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync()
+      console.log(photo)
+      console.log(photo.base64?.length)
+      const response = await axios.post('http://localhost:8080/items', {
+        image: photo.base64,
+      })
+      console.log(response.data)
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.flipCameraContainer}>
+        <TouchableOpacity onPress={toggleCameraType}>
+          <Text style={styles.text}>Flip Camera</Text>
+        </TouchableOpacity>
+      </View>
+      <Camera style={styles.camera} type={type} ref={cameraRef}></Camera>
+      <PhotoButton onPress={takePicture} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    camera: {
-        flex: 1,
-    },
-    text: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flipCameraContainer: {
+    marginBottom: 40,
+  },
+  camera: {
+    height: 640,
+    width: 480,
+    borderRadius: 20,
+  },
+  text: {
+    flex: 1,
+  },
 })
